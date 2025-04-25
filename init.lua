014ed8b8f4ee6dfe6939197a6c617cecdae8aee1
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-04-25 22:48:22 +0800

    feat(config): configured neovim and lazy.nvim.

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
+vim.wo.cursorline = true
+-- Display tabs and trailing spaces
+vim.opt.list = true
+vim.opt.listchars = { tab = ">-", trail = "-" }
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 
 require("config.lazy") -- Import `./lua/config/lazy.lua`
