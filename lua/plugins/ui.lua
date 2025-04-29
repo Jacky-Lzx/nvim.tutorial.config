c0030ba4072af28c1d50739eb091ef388c0f7227
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-04-29 16:02:55 +0800

    feat(install): installed plugins for chapter 2.

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+       'nvim-tree/nvim-web-devicons'
+    },
+    opts = {}
+  },
+
+  {
+    'romgrk/barbar.nvim',
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    dependencies = {
+      'lewis6991/gitsigns.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {},
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {}
+  },
+
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    main = "rainbow-delimiters.setup",
+    submodules = false,
+    opts = {}
+  },
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    opts = {}
+  }
+}
