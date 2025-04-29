d84bcbd1e63d6d8e01794241c9bab6c8ac05d4ff
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-04-29 21:53:45 +0800

    feat(config): configured plugins for chapter 2.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,7 +4,60 @@ return {
     dependencies = {
        'nvim-tree/nvim-web-devicons'
     },
-    opts = {}
+    opts = {
+      options = {
+        theme = "catppuccin",
+        always_divide_middle = false,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = { "encoding", "fileformat", "filetype", "progress" },
+        lualine_z = { "location" },
+      },
+      winbar = {
+        lualine_a = {
+          "filename"
+        },
+        lualine_b = {
+          { function() return " " end, color = 'Comment'},
+        },
+        lualine_x = {
+          "lsp_status"
+        }
+      },
+      inactive_winbar = {
+        -- Always show winbar
+        lualine_b = { function() return " " end },
+      },
+    },
+    config = function(_, opts)
+      local mocha = require("catppuccin.palettes").get_palette("mocha")
+
+      local function show_macro_recording()
+        local recording_register = vim.fn.reg_recording()
+        if recording_register == "" then
+          return ""
+        else
+          return "󰑋 " .. recording_register
+        end
+      end
+
+      local macro_recording = {
+        show_macro_recording,
+        color = { fg = "#333333", bg = mocha.red },
+        separator = { left = "", right = "" },
+        padding = 0,
+      }
+
+      table.insert(opts.sections.lualine_x, 1, macro_recording)
+
+      require("lualine").setup(opts)
+    end
   },
 
   {
@@ -15,7 +68,34 @@ return {
       'nvim-tree/nvim-web-devicons',
     },
     init = function() vim.g.barbar_auto_setup = false end,
-    opts = {},
+    event = { "VeryLazy" },
+    keys = {
+      { "<M-<>", "<CMD>BufferMovePrevious<CR>", mode = {"n"}, desc = "[Buffer] Move buffer left"  },
+      { "<M->>", "<CMD>BufferMoveNext<CR>",     mode = {"n"}, desc = "[Buffer] Move buffer right" },
+      { "<M-1>", "<CMD>BufferGoto 1<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 1"    },
+      { "<M-2>", "<CMD>BufferGoto 2<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 2"    },
+      { "<M-3>", "<CMD>BufferGoto 3<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 3"    },
+      { "<M-4>", "<CMD>BufferGoto 4<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 4"    },
+      { "<M-5>", "<CMD>BufferGoto 5<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 5"    },
+      { "<M-6>", "<CMD>BufferGoto 6<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 6"    },
+      { "<M-7>", "<CMD>BufferGoto 7<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 7"    },
+      { "<M-8>", "<CMD>BufferGoto 8<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 8"    },
+      { "<M-9>", "<CMD>BufferGoto 9<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 9"    },
+      { "<M-h>", "<CMD>BufferPrevious<CR>",     mode = {"n"}, desc = "[Buffer] Previous buffer"   },
+      { "<M-l>", "<CMD>BufferNext<CR>",         mode = {"n"}, desc = "[Buffer] Next buffer"       },
+      { "<M-w>", "<CMD>BufferClose<CR>",        mode = {"n"}, desc = "Close buffer"               },
+    },
+    opts = {
+      animation = false,
+      -- Automatically hide the tabline when there are this many buffers left.
+      -- Set to any value >=0 to enable.
+      auto_hide = 1,
+
+      -- Set the filetypes which barbar will offset itself for
+      sidebar_filetypes = {
+        NvimTree = true, -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+      },
+    },
   },
 
   {
@@ -23,6 +103,9 @@ return {
     version = "*",
     dependencies = {
       "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "<leader>e", "<CMD>NvimTreeToggle<CR>", mode = {"n"}, desc = "[NvimTree] Toggle NvimTree" },
     },
     opts = {}
   },
@@ -43,8 +126,39 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
+      {"rcarriga/nvim-notify", opts = {background_colour = "#000000"}}
     },
-    opts = {}
+    keys = {
+      { "<leader>sN", "<CMD>Noice pick<CR>", desc = "[Noice] Pick history messages" }, -- FIXME: Currently unusable
+      { "<leader>N", "<CMD>Noice<CR>", desc = "[Noice] Show history messages" },
+    },
+
+    opts = {
+      popupmenu = {
+        enabled = false,
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+      routes = {
+        -- Hide search count
+        { filter = { event = "msg_show", kind = "search_count", }, opts = { skip = true }, },
+        -- Hide written message
+        { filter = { event = "msg_show", kind = "", }, opts = { skip = true }, },
+      },
+    }
   }
 }
