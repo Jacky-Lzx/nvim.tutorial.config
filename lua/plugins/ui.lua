06e285cad88433e3ecd69e8d82c2843305661d01
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-05-08 12:42:33 +0800

    feat(which-key): changed configs of which-key.nvim.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -204,20 +204,30 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      ---@type false | "classic" | "modern" | "helix"
+      preset = "helix",
+      win = {
+        -- no_overlap = true,
+        title = false,
+        width = 0.5,
+      },
+      -- stylua: ignore
+      spec = {
+        { "<leader>cc", group = "<CodeCompanion>", icon = "" },
+        { "<leader>s",  group = "<Snacks>"                    },
+        { "<leader>t",  group = "<Snacks> Toggle"             },
+      },
+      -- expand all nodes wighout a description
+      expand = function(node)
+        return not node.desc
+      end,
     },
     keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
+      -- stylua: ignore
+      { "<leader>?", function() require("which-key").show({ global = false }) end, desc = "[Which-key] Buffer Local Keymaps", },
     },
-  }
+  },
 
 }
