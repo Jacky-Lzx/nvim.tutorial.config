295f74112b9cdc26b2f723e1864236aa4fa89e06
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-05-08 12:39:10 +0800

    feat(snacks): changed animation configs.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -22,5 +22,47 @@ return {
       statuscolumn = { enabled = true },
       words = { enabled = true },
     },
+
+    init = function()
+        local Snacks = require("snacks")
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "VeryLazy",
+          callback = function()
+            -- Setup some globals for debugging (lazy-loaded)
+            _G.dd = function(...)
+              Snacks.debug.inspect(...)
+            end
+            _G.bt = function()
+              Snacks.debug.backtrace()
+            end
+            vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+            vim.g.snacks_animate = false
+            Snacks.toggle.new({
+              id = "Animation",
+              name = "Animation",
+              get = function()
+                return Snacks.animate.enabled()
+              end,
+              set = function(state)
+                vim.g.snacks_animate = state
+              end
+            }):map("<leader>ta")
+
+            -- Create some toggle mappings
+            Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>ts")
+            Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>tw")
+            Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>tL")
+            Snacks.toggle.diagnostics():map("<leader>td")
+            Snacks.toggle.line_number():map("<leader>tl")
+            Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>tc")
+            Snacks.toggle.treesitter():map("<leader>tT")
+            Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>tb")
+            Snacks.toggle.inlay_hints():map("<leader>th")
+            Snacks.toggle.indent():map("<leader>tg")
+            Snacks.toggle.dim():map("<leader>tD")
+          end,
+        })
+      end,
   }
 }
