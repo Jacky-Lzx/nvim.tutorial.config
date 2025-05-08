7e690052f3a73dcef4f1bbfce94297e798e9acfc
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-05-08 13:01:39 +0800

    feat(snacks): changed dim configs.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -50,6 +50,8 @@ return {
             }):map("<leader>ta")
 
             -- Create some toggle mappings
+            Snacks.toggle.dim():map("<leader>tD")
+
             Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>ts")
             Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>tw")
             Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>tL")
