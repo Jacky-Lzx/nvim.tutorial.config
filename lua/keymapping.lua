ec460f1c4a7567a25642d7094a47f95d2d5b98ac
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-05-11 10:33:30 +0800

    fix(keymapping): fixed the mappings of <S-H> and <S-L> so that they work for `y`, `d`, and `c` commands, etc. Closed #1.

--- lua/keymapping.lua
+++ lua/keymapping.lua
@@ -10,13 +10,10 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 
-vim.keymap.set({ "n", "x" }, "<S-H>", "^", { desc = "Start of line" })
-vim.keymap.set({ "n", "x" }, "<S-L>", "$", { desc = "End of line" })
-vim.keymap.set("o", "<S-H>", "^", { desc = "Start of line" })
-vim.keymap.set("o", "<S-L>", "$", { desc = "End of line" })
+vim.keymap.set({ "n", "x", "o" }, "<S-H>", "^", { desc = "Start of line" })
+vim.keymap.set({ "n", "x", "o" }, "<S-L>", "$", { desc = "End of line" })
 
 vim.keymap.set({ "n", "x" }, "Q", "<CMD>:qa<CR>")
 vim.keymap.set({ "n", "x" }, "qq", "<CMD>:q<CR>")
 
 vim.keymap.set("n", "<A-z>", "<CMD>set wrap!<CR>", { desc = "Toggle line wrap" })
-
