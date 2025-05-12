789bec51b90df16832c054316e897c828db20989
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-05-12 10:35:56 +0800

    feat(isntall): installed smarkyank.nvim.

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -48,4 +48,20 @@ return {
     config = true,
   },
 
+  {
+    "ibhagwan/smartyank.nvim",
+    event = { "BufWinEnter" },
+    opts = {
+      highlight = {
+        timeout = 500, -- timeout for clearing the highlight
+      },
+      clipboard = {
+        enabled = true,
+      },
+      osc52 = {
+        silent = true, -- true to disable the "n chars copied" echo
+      },
+    },
+  },
+
 }
